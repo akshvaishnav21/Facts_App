5d270507d2738c6b9319d0d6767460db438f03f9
akshvaishnav2194 <akshvaishnav2194@gmail.com>
2015-12-31 23:53:53 +0530

    Removed some facts

--- app/src/main/java/com/teamtreehouse/funfacts/FactBook.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FactBook.java
@@ -10,9 +10,7 @@ public class FactBook {
             "Olympic gold medals are actually made mostly of silver.",
             "You are born with 300 bones; by the time you are an adult you will have 206.",
             "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
+
             "According to a recent survey lol is the most overused word.",
             "Whatever is the most annoying word according to a recent study.",
             "Banging your head against a wall burns 150 calories an hour.",
